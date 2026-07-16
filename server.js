f95c6e9742b9694922b8e02e2c0263a03c9fc5b9
Abdul Rehman <abdulrehman@digitalocean.com>
2026-07-17 00:46:40 +0500

    First commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { client } = require('./lib/contentful');
+const { documentToHtmlString } = require('@contentful/rich-text-html-renderer');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await client.getEntries({ content_type: 'pageBlogPost' });
+
+    const posts = response.items.map((post) => ({
+      title: post.fields.title,
+      contentHtml: post.fields.content
+        ? documentToHtmlString(post.fields.content)
+        : null,
+    }));
+
+    res.render('index', { posts });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Something went wrong fetching posts.');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
