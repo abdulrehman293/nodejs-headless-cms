8261fddfec0cff280d73e06c4d16e3cd6a57bcf9
Abdul Rehman <abdulrehman@digitalocean.com>
2026-07-17 13:13:13 +0500

    Fix views and static path resolution

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { client } = require('./lib/contentful');
 const { documentToHtmlString } = require('@contentful/rich-text-html-renderer');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async (req, res) => {
   try {
